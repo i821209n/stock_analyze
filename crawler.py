f4ff89e27350d5fcbc90b228d17406b2c65321ea
ian2_lin <Ian2_Lin@pegatroncorp.com>
2023-11-29 17:38:01 +0800

    complete get_data api 1. use selenium to crawl web 2. save data to local csv file

--- crawler.py
+++ crawler.py
@@ -1,6 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+import os
 
 def isfloat(num):
     try:
@@ -16,27 +22,60 @@ def isint(num):
         return False
 
 def get_data(stock_num):
-    url = f"https://goodinfo.tw/StockInfo/StockBzPerformance.asp?STOCK_ID={stock_num}"
-    headers = {
-        "user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
-    }
-    try:
-        res = requests.get(url, headers=headers)
-    except:
-        print(f"wrong url : {url}")
-        return
-    res.encoding = "utf-8"
+    start_time = time.time()
+    if not os.path.exists('csv_file.csv'):
+        # Set up Chrome options for headless mode
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # Run Chrome in headless mode
+        # chrome_options.add_argument('--disable-gpu')  # Disable GPU acceleration (necessary in headless mode)
 
-    soup = BeautifulSoup(res.text, "lxml")
-    print(f"Title : {soup.title.text}")
-    # link_tag = soup.find_all('a', class_='link_blue')
-    # print(link_tag)
+        url = f"https://goodinfo.tw/StockInfo/StockBzPerformance.asp?STOCK_ID={stock_num}"
+        # headers = {
+        #     "user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+        # }
 
-    data = soup.select_one("#txtFinDetailData")
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.implicitly_wait(10)
+        try:
+            driver.get(url)
+            # res = requests.get(url, headers=headers)
+        except:
+            print(f"wrong url : {url}")
+            return
+        # res.encoding = "utf-8"
+        print(driver.title)
 
-    dfs = pd.read_html((data.prettify()))
+        # Find the select element with a specific onchange attribute
+        select_css_selector = 'select[onchange="ChgFinDetailSheet(this.value);"]'
+        select_element = driver.find_element(By.CSS_SELECTOR, select_css_selector)
 
-    df = dfs[0]
+        # Now, you can interact with the select element as needed
+        # For example, you can change the selected option
+        select = Select(select_element)
+        select.select_by_index(3)  # Replace with the desired option index
+        time.sleep(1)
 
-    # print(df)
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        # print(f"Title : {soup.title.text}")
+        # link_tag = soup.find_all('a', class_='link_blue')
+        # print(link_tag)
+
+        data = soup.select_one("#txtFinDetailData")
+
+        dfs = pd.read_html((data.prettify()))
+
+        df = dfs[0]
+
+        # print(df)
+
+        driver.quit()
+
+        df.to_csv('csv_file.csv', index=False)
+        # df.to_excel('excel_file.xlsx')
+
+    # Read the CSV file into a DataFrame
+    df1 = pd.read_csv('csv_file.csv')
+    # print(df1)
+
+    print(f"running time : {time.time() - start_time}")
 
