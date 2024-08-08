b3e3276756abf98d77a557df56c5328b27552a62
C.Y.Lin <83861620+i821209n@users.noreply.github.com>
2024-08-08 22:06:47 +0800

    Using post request to get revenue from "mops.twse.com.tw" successful

--- twse_test/twse_revenue_test.py
+++ twse_test/twse_revenue_test.py
@@ -2,15 +2,15 @@ import requests
 import pandas as pd
 
 # 取得資料
-url = 'https://openapi.twse.com.tw/v1/opendata/t187ap05_P'
-response = requests.get(url)
-data = response.json()
+# url = 'https://openapi.twse.com.tw/v1/opendata/t187ap05_P'
+# response = requests.get(url)
+# data = response.json()
 
 # 將資料轉為 DataFrame 方便處理
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
 
 # 篩選出台積電的資料
-tsmc_data = df[df['公司代號'] == '2330']
+# tsmc_data = df[df['公司代號'] == '2330']
 
 # 顯示結果
 # for i in range len(tsmc_data):
@@ -19,5 +19,55 @@ tsmc_data = df[df['公司代號'] == '2330']
 # 打印第一行的所有值
 # print(tsmc_data.iloc[0, :])
 
-keys_list = list(df.columns)
-print(keys_list)
+# keys_list = list(df.columns)
+# print(keys_list)
+
+# 設定請求的URL和參數
+url = 'https://mops.twse.com.tw/mops/web/ajax_t05st10_ifrs'
+payload = {
+    'encodeURIComponent': 1,
+    'step': 1,
+    'firstin': 1,
+    'off': 1,
+    'keyword4': '',
+    'code1': '',
+    'TYPEK2': '',
+    'checkbtn': '',
+    'queryName': 'co_id',
+    'inpuType': 'co_id',
+    'TYPEK': 'all',
+    'isnew': 'false',
+    'co_id': '2330',
+    'year': '113',
+    'month': '01'
+}
+
+# 發送POST請求
+response = requests.post(url, data=payload)
+
+# 確認請求成功
+if response.status_code == 200:
+    # 使用pandas從HTML表格中提取數據
+    tables = pd.read_html(response.text)
+
+    # 如果有多個表格，根據實際需求選擇需要的表格
+    if tables:
+        # 檢查提取的表格數量
+        print(f"提取的表格數量: {len(tables)}")
+
+        # 顯示提取的表格
+        print("----------------------------")
+        for i, table in enumerate(tables):
+            print(f"表格 {i+1}:\n{table}\n")
+            print("----------------------------")
+
+        df = tables[1]  # 假設我們需要第一個表格
+
+        result = df[df["項目"] == "本月"]["營業收入淨額"].values[0]
+        print(f"本月營收 : {result} (單位：新台幣仟元)")
+
+    else:
+        print("未找到任何表格數據。")
+else:
+    print(f"請求失敗，狀態碼: {response.status_code}")
+
